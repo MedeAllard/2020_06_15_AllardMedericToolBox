771584af82f4118fa09d0e152a3b50cec7e18ddd
Allard Mederic <medericallardisf@gmail.com>
2020-06-18 15:33:13 +0200

    Say hello

--- Runtime/HandMovement.cs
+++ Runtime/HandMovement.cs
@@ -1,21 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HandMovement : MonoBehaviour
 {
+    public Transform m_head;
+    public Transform m_hand;
+
+    public bool leftRightBoolean;
+    public bool previousLeftRightBoolean;
+    public int counter;
+    public float timeCountdown;
+
+    public UnityEvent onStartWaving;
+    public UnityEvent onEndWaving;
+
+    private bool angleMin = false;
+
     private bool m_check;
 
     public bool Check { get => m_check; set => m_check = value; }
 
-    public void LaunchCoroutine()
+    private void Update()
     {
-       // if(Check == false)
-            //StartCoroutine(CheckMovementHello());
+        if(m_check)
+        {
+            Vector3 localDirection = (m_head.InverseTransformPoint(m_hand.position)).normalized;
+            leftRightBoolean = localDirection.x > 0;
+            if (previousLeftRightBoolean != leftRightBoolean)
+            {
+                counter++;
+                timeCountdown = 0.5f;
+                if (counter == 3)
+                {
+                    onStartWaving.Invoke();
+                }
+            }
+            if(timeCountdown > 0)
+            {
+                timeCountdown -= Time.deltaTime;
+            }
+            if(timeCountdown < 0)
+            {
+                onEndWaving.Invoke();
+                counter = 0;
+                timeCountdown = 0;
+            }
+            previousLeftRightBoolean = leftRightBoolean;
+        }
     }
-
-    /*IEnumerator CheckMovementHello()
-    {
-        yield return WaitForSeconds(2);
-    }*/
 }
