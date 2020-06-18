63bd2bd9b4bfa18c7d88444257521af6c5a52071
Allard Mederic <medericallardisf@gmail.com>
2020-06-18 10:44:53 +0200

    RAYCAST

---
+++ Runtime/Raycast.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Raycast : MonoBehaviour
+{
+    public LayerMask _layer;
+
+    public float _distance;
+
+    RaycastHit _raycastHit;
+
+    public UnityEvent OnCastHit;
+    public UnityEvent OnCastMiss;
+
+    public void Update()
+    {
+        Cast();
+    }
+
+    public void Cast()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 100, Color.red);
+        if (Physics.Raycast(transform.position, transform.forward, out _raycastHit, _distance, _layer))
+        {
+            OnCastHit.Invoke();
+        }
+        else
+        {
+            OnCastMiss.Invoke();
+        }
+    }
+
+    protected bool CheckLayer(int layer)
+    {
+        return _layer == (_layer | (1 << layer));
+    }
+}
